54f03ce9a9baa194d63fbbb13c03d9f531446278
dominik <dominik.lovric4@gmail.com>
2026-07-30 23:45:27 +0200

    Commit #8: 	Add font style service for font size and future font support

---
+++ lib/services/text_style_service.dart
@@ -0,0 +1,43 @@
+  import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+  import 'package:shared_preferences/shared_preferences.dart';
+  
+  import 'package:dnd_app/services/settings_service.dart';
+
+  class TextStyleService {
+
+    static final ChangeNotifier themeNotifier = ChangeNotifier();
+
+    static List<String> sizes=["Header", "Category", "text", "additional"];
+    static List<double> baseSizes=[35,30,20,10];
+
+    static double getFontSize(int size){
+      double? setting = SettingsService.getSetting(sizes[size]);
+      if(setting!=null){
+        return setting;
+      }
+      return baseSizes[size];
+    }
+
+    static Future<void> setFontSize(int sizeName, double size) async{
+      await SettingsService.setSetting(sizes[sizeName], size);
+
+      themeNotifier.notifyListeners();
+    }
+
+    static List<String> getFontSizeNames(){
+      return sizes;
+    }
+
+    Future<void> resetFontSize(int sizeName) async{
+      await SettingsService.setSetting( sizes[sizeName], getBasicSize(sizeName));
+    }
+
+    static double getBasicSize(int size){
+      if(size<sizes.length){
+        return baseSizes[size];
+      }
+      return 0;
+    }
+
+  }
