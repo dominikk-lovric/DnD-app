625514533a28ce82cfb0307a36da3cde2f9f1c1b
dominik <dominik.lovric4@gmail.com>
2026-09-15 22:58:32 +0200

    Commit #48: 	Add get choice selector for adding multiple choice options in classes 	Add buildInfoWidget for quick lookup of selection

--- lib/widgets/character_creator_widget.dart
+++ lib/widgets/character_creator_widget.dart
@@ -1,6 +1,9 @@
 import 'package:dnd_app/services/color_service.dart';
 import 'package:dnd_app/services/json_service.dart';
+import 'package:dnd_app/services/settings_service.dart';
 import 'package:dnd_app/services/text_style_service.dart';
+import 'package:dnd_app/widgets/description_column_widget.dart';
+import 'package:dnd_app/widgets/description_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,18 +17,23 @@ class CharacterCreatorWidget extends ConsumerStatefulWidget {
 
 class CharacterCreatorWidgetState
     extends ConsumerState<CharacterCreatorWidget> {
+  Map<String, dynamic> selectedMap = {};
   late final TextStyleController textStyleController;
+  late final SettingsController settingsController;
   late final ColorController colorController;
   Map<String, dynamic> categoryData = {};
   Map<String, dynamic> classData = {};
   Map<String, dynamic> speciesData = {};
   Map<String, dynamic> backgroundData = {};
+  Map<String, dynamic> schemata = {};
+  int classesNum = 1;
   int level = 0;
   bool loaded = false;
   @override
   initState() {
     loaded = false;
     super.initState();
+    settingsController = ref.read(settingsControllerProvider.notifier);
     textStyleController = ref.read(textStyleControllerProvider.notifier);
     colorController = ref.read(colorControllerProvider.notifier);
     loadData();
@@ -44,12 +52,18 @@ class CharacterCreatorWidgetState
     Map<String, dynamic> backgrounds = await JsonService.loadFromPath(
       categories["backgrounds"]["path"],
     );
+    Map<String, dynamic> schema = await JsonService.loadFromPath("schemata");
     setState(() {
       categoryData = categories;
       classData = classes;
       speciesData = species;
       backgroundData = backgrounds;
+      schemata = schema;
       loaded = true;
+      selectedMap["class1"] = selectedMap["class1"] ?? classData.keys.first;
+      selectedMap["species"] = selectedMap["species"] ?? speciesData.keys.first;
+      selectedMap["backgrounds"] =
+          selectedMap["backgrounds"] ?? backgroundData.keys.first;
     });
   }
 
@@ -59,6 +73,7 @@ class CharacterCreatorWidgetState
     if (!loaded) {
       return Center(child: CircularProgressIndicator());
     }
+    print(selectedMap);
     return Form(
       key: _creatorKey,
       child: Column(
@@ -104,7 +119,6 @@ class CharacterCreatorWidgetState
                   : "Level must be between 1 and 20";
             },
             onChanged: (value) {
-              print(value);
               try {
                 level = int.parse(value);
               } catch (e) {
@@ -113,42 +127,163 @@ class CharacterCreatorWidgetState
               print(level);
             },
           ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.sizeOf(context).height / 144,
+              vertical: MediaQuery.sizeOf(context).height / 72,
+            ),
+            decoration: BoxDecoration(
+              color: colorController.getColor(3),
+              borderRadius: BorderRadius.circular(
+                MediaQuery.sizeOf(context).height / 72,
+              ),
+            ),
+            child: Column(
+              spacing: MediaQuery.sizeOf(context).height / 72,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Class",
+                      style: textStyleController.getTextStyle(5, 4),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        classesNum++;
+                        selectedMap["class".toString() +
+                                classesNum.toString()] =
+                            selectedMap["class".toString() +
+                                classesNum.toString()] ??
+                            classData.keys.first;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.add, color: colorController.getColor(4)),
+                    ),
+                  ],
+                ),
+                ...selectedMap.keys.toList().map((id) {
+                  if (id.startsWith("class"))
+                    return getChoiceSelector(
+                      classData,
+                      id,
+                      "classes",
+                      level: true,
+                    );
+                  return SizedBox.shrink();
+                }),
+              ],
+            ),
+          ),
+          getChoiceSelector(speciesData, "species", "species"),
+          getChoiceSelector(backgroundData, "backgrounds", "backgrounds"),
         ],
       ),
     );
   }
 
-  Widget getChoiceSelector(Map<String, dynamic> options, {int initial = 0}) {
+  Widget getChoiceSelector(
+    Map<String, dynamic> options,
+    String selectorId,
+    String category, {
+    bool level = false,
+  }) {
     List<String> keys = options.keys.toList();
-    String _selected = options[keys[0]]["name"];
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Expanded(
-          child: DropdownButtonFormField(
-            value: _selected,
-            decoration: InputDecoration(
-              filled: true,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).height / 144,
+        vertical: 0,
+      ),
+      decoration: BoxDecoration(
+        color: colorController.getColor(2),
+        borderRadius: BorderRadius.circular(
+          MediaQuery.sizeOf(context).height / 72,
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              selectedMap.remove(selectorId);
+              setState(() {});
+            },
+            icon: Icon(Icons.cancel, color: colorController.getColor(4)),
+          ),
+          if (level)
+            Flexible(
+              flex: 1,
+              child: TextFormField(
+                maxLength: 2,
+                validator: (value) {
+                  return (int.parse(value ?? "0") <= 20 &&
+                          int.parse(value ?? "0") > 0)
+                      ? null
+                      : "Level must be between 1 and 20";
+                },
               ),
             ),
-            items: keys
-                .map(
-                  (item) => DropdownMenuItem(
-                    value: item,
-                    child: Text(options[item]["name"]),
+
+          Flexible(
+            flex: 4,
+            child: DropdownButtonFormField(
+              icon: const SizedBox.shrink(),
+              initialValue: selectedMap[selectorId],
+              dropdownColor: colorController.getColor(3),
+              decoration: InputDecoration(
+                suffixIcon: null,
+                filled: true,
+                fillColor: Colors.transparent,
+                border: InputBorder.none,
+              ),
+              items: keys.map((item) {
+                String i = item;
+                return DropdownMenuItem(
+                  value: i,
+                  child: Text(
+                    options[item]["name"].toString(),
+                    style: textStyleController.getTextStyle(6, 4),
                   ),
-                )
-                .toList(),
-            onChanged: (value) {
-              setState(() => _selected = value ?? "");
-            },
+                );
+              }).toList(),
+              onChanged: (value) async {
+                setState(() => selectedMap[selectorId] = value ?? "");
+              },
+            ),
           ),
-        ),
-        const SizedBox(width: 8),
-        IconButton.filled(onPressed: () {}, icon: Icon(Icons.circle)),
-      ],
+          DescriptionWidget(
+            options[selectedMap[selectorId]]["name"].toString(),
+            buildInfoWidget(options[selectedMap[selectorId]]["json"]),
+            category + "DescriptionStyle",
+            clickWidget: Icon(
+              Icons.open_in_new,
+              color: colorController.getColor(4),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildInfoWidget(String item) {
+    print(item);
+    return FutureBuilder<dynamic>(
+      future: JsonService.loadFromPath(item),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Text("Error loading data: ${snapshot.error}");
+        }
+
+        if (!snapshot.hasData) {
+          return const SizedBox.shrink();
+        }
+
+        final infoData = Map<String, dynamic>.from(snapshot.data as Map);
+        return DescriptionColumnWidget(infoData, schemata);
+      },
     );
   }
 }
